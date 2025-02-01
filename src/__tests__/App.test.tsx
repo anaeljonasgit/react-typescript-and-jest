4bf3cfe8149757aa99ac063c2b03bf8d3095db27
Your Name <anaeljonas@outlook.com>
2025-02-01 20:33:06 -0300

    feature: configure babel and jest

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,11 @@
+import { render } from "@testing-library/react";
+
+import App from "../App";
+
 describe("Render App", () => {
   it("Render App", () => {
+    render(<App />);
+
     expect(true).toBe(true);
   });
 });
